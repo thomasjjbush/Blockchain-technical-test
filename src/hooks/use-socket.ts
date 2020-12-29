b385bcea1c46a8600e583299943834afea08f7a9
Thomas Bush <thomasjjbush@gmail.com>
2020-12-29 00:54:13 +0000

    define ref inside use effect rather than directly to avoid new connections

--- src/hooks/use-socket.ts
+++ src/hooks/use-socket.ts
@@ -3,15 +3,17 @@ import { useDispatch } from 'react-redux';
 import { SocketOptions } from '../../types';
 
 export const useSocket = <T>(url: string, options?: SocketOptions<T>): void => {
-    const { current: socket } = useRef(new WebSocket(url));
+    const socket = useRef<WebSocket>();
     const dispatch = useDispatch();
 
     useEffect(() => {
-        socket.onclose = () => socket.send(JSON.stringify({ op: options.onClose }));
-        socket.onerror = () => dispatch({ type: options.onError });
-        socket.onopen = () => socket.send(JSON.stringify({ op: options.onOpen }));
-        socket.onmessage = (e) => dispatch({ type: options.onMessage, payload: JSON.parse(e.data).x });
+        socket.current = new WebSocket(url);
 
-        return () => socket.close();
+        socket.current.onclose = () => socket.current.send(JSON.stringify({ op: options.onClose }));
+        socket.current.onopen = () => socket.current.send(JSON.stringify({ op: options.onOpen }));
+        socket.current.onerror = () => dispatch({ type: options.onError });
+        socket.current.onmessage = (e) => dispatch({ type: options.onMessage, payload: JSON.parse(e.data).x });
+
+        return () => socket.current.close();
     }, []);
 };
